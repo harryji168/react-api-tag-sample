d6688dbcd0e9f24e0229412d4623d3f9257b5a24
Harry Ji <jiharry@hotmail.com>
2022-02-16 22:03:44 -0800

    deploy

---
+++ precache-manifest.77436c54fc6c0d5fa6a500b6f8c5151e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c64baa0923e27ee89992572818722b5",
+    "url": "/react-api-tag-sample/index.html"
+  },
+  {
+    "revision": "62b9e08dd84827d4e6c8",
+    "url": "/react-api-tag-sample/static/css/main.fc99204d.chunk.css"
+  },
+  {
+    "revision": "1407c86a95fc80bc2054",
+    "url": "/react-api-tag-sample/static/js/2.8ed737be.chunk.js"
+  },
+  {
+    "revision": "62b9e08dd84827d4e6c8",
+    "url": "/react-api-tag-sample/static/js/main.456c087f.chunk.js"
+  },
+  {
+    "revision": "c0e17f3fed5791e79c77",
+    "url": "/react-api-tag-sample/static/js/runtime~main.875a1312.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-api-tag-sample/precache-manifest.77436c54fc6c0d5fa6a500b6f8c5151e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-api-tag-sample/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
